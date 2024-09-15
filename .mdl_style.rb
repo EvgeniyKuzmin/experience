ef32f3b665e352e6b813a390b57c7074786ceb53
Evgenii Kuzmin <evgenii.a.kuzmin@gmail.com>
2024-09-15 13:04:47 +0300

    Added mdl validation

---
+++ .mdl_style.rb
@@ -0,0 +1,8 @@
+all
+rule 'MD013', :tables => false;
+rule 'MD013', :line_length => -1;
+rule 'MD013', :ignore_code_blocks => true;
+rule 'MD026', :punctuation => ".,;:";
+rule 'MD004', :style => :dash;
+rule 'MD029', :style => :ordered;
+rule 'MD046', :style => :consistent;
